d4ff08a9f6135bbeb74501ce6315d050c9f772c2
Hassan <hassanhauda@gmail.com>
2024-07-31 16:47:16 +0100

    Add initial project configuration files and dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  settings: {
+    react: { version: "detect" },
+    "import/resolver": {
+      node: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    },
+  },
+  plugins: [
+    "react-refresh",
+    "react",
+    "react-hooks",
+    "jsx-a11y",
+    "import",
+    "prettier",
+    "@typescript-eslint",
+  ],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/jsx-uses-react": "off",
+    "react/jsx-uses-vars": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+};
